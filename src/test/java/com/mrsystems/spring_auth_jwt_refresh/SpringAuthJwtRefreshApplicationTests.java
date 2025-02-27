27f4fbfd081b1a899be9e7e11b62738b2d29dbb8
MaikRibeiro <michael.douglas2020@hotmail.com>
2025-02-27 00:08:36 -0300

    Initial commit

---
+++ src/test/java/com/mrsystems/spring_auth_jwt_refresh/SpringAuthJwtRefreshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrsystems.spring_auth_jwt_refresh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthJwtRefreshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
