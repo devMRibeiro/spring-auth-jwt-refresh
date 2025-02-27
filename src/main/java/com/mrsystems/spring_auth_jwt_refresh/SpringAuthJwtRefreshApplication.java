0468459db3fa472a78fd938feb4bdc23cf6589a6
MaikRibeiro <michael.douglas2020@hotmail.com>
2025-02-27 00:08:36 -0300

    Initial commit

---
+++ src/main/java/com/mrsystems/spring_auth_jwt_refresh/SpringAuthJwtRefreshApplication.java
@@ -0,0 +1,13 @@
+package com.mrsystems.spring_auth_jwt_refresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthJwtRefreshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthJwtRefreshApplication.class, args);
+	}
+
+}
