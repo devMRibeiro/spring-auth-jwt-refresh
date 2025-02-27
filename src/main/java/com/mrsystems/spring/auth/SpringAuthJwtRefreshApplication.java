f8f55b42720d37f0c0c2eb1ebf4fef5b5e8a5177
MaikRibeiro <michael.douglas2020@hotmail.com>
2025-02-27 02:12:05 -0300

    Create models and Enum to Role

--- src/main/java/com/mrsystems/spring/auth/SpringAuthJwtRefreshApplication.java
+++ src/main/java/com/mrsystems/spring/auth/SpringAuthJwtRefreshApplication.java
@@ -1,4 +1,4 @@
-package com.mrsystems.spring_auth_jwt_refresh;
+package com.mrsystems.spring.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
