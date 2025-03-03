d083d5882fee9a92bd4f8aea8602ccb42b15fd83
MaikRibeiro <michael.douglas2020@hotmail.com>
2025-03-03 03:07:08 -0300

    Create JWT Utility class

---
+++ src/main/java/com/mrsystems/spring/auth/security/jwt/JwtUtils.java
@@ -0,0 +1,99 @@
+package com.mrsystems.spring.auth.security.jwt;
+
+import java.security.Key;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.WebUtils;
+
+import com.mrsystems.spring.auth.security.service.UserDetailsImpl;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Component
+public class JwtUtils {
+	private static final Logger log = LoggerFactory.getLogger(JwtUtils.class);
+
+	@Value("${MIRASYSTEMS.APP.JWTSECRET}")
+	private String jwtScret;
+
+	@Value("${MIRASYSTEMS.APP.JWTEXPIRATIONMS}")
+	private int jwtExpirationMs;
+	
+	@Value("${MIRASYSTEMS.APP.JWTCOOKIENAME}")
+	private String jwtCookie;
+
+	public String getJwtFromCookies(HttpServletRequest request) {
+		Cookie cookie = WebUtils.getCookie(request, jwtCookie);
+
+		return (cookie != null) ? cookie.getValue() : null;
+	}
+	
+	public ResponseCookie generateJwtCookie(UserDetailsImpl userPrincipal) {
+		String jwt = generateTokenFromUsername(userPrincipal.getUsername());
+
+		return ResponseCookie.from(jwtCookie, jwt)
+				.path("/api").maxAge(24 * 60 * 60)
+				.httpOnly(true)
+				.build();
+	}
+
+	public ResponseCookie getCleanJwtCookie() {
+		return ResponseCookie.from(jwtCookie)
+				.path("/api")
+				.build();
+	}
+
+	public String getUsernameFromJwtToken(String token) {
+		return Jwts
+				.parserBuilder()
+				.setSigningKey(key())
+				.build()
+				.parseClaimsJws(token)
+				.getBody()
+				.getSubject();
+	}
+
+	public Key key() {
+		return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtScret));
+	}
+
+	public boolean validateJwtToken(String authToken) {
+	
+		try {
+			Jwts.parserBuilder().setSigningKey(key()).build().parse(authToken);
+			return true;
+		} catch (MalformedJwtException e) {
+			log.error("Invalid JWT token: {}", e.getMessage());
+		} catch (ExpiredJwtException e) {
+			log.error("JWT token is expired: {}", e.getMessage());
+		} catch (UnsupportedJwtException e) {
+			log.error("JWT token is unssuported: {}", e.getMessage());
+		} catch (IllegalArgumentException e) {
+			log.error("JWT claims string is empty: {}", e.getMessage());
+		}
+
+		return false;
+	}
+	
+	public String generateTokenFromUsername(String username) {
+		return Jwts.builder()
+				.setSubject(username)
+				.setIssuedAt(new Date())
+				.setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+				.signWith(key(), SignatureAlgorithm.HS256)
+				.compact();
+	}
+}
