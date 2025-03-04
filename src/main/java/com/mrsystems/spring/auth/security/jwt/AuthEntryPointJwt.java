2ffe9f678ae8583ac75028d3dde3c71319aa414d
MaikRibeiro <michael.douglas2020@hotmail.com>
2025-03-04 20:50:04 -0300

    Customize response data

--- src/main/java/com/mrsystems/spring/auth/security/jwt/AuthEntryPointJwt.java
+++ src/main/java/com/mrsystems/spring/auth/security/jwt/AuthEntryPointJwt.java
@@ -1,23 +1,37 @@
 package com.mrsystems.spring.auth.security.jwt;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
-
 	private static final Logger log = LoggerFactory.getLogger(AuthenticationEntryPoint.class);
 
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
 		log.error("Unauthorized error: {}", authException.getMessage());
-		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
+
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+		HashMap<String, Object> body = new HashMap<String, Object>();
+		body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
+		body.put("error", "Unauthorized");
+		body.put("message", authException.getMessage());
+		body.put("path", request.getServletPath());
+
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.writeValue(response.getOutputStream(), body);
 	}
 }
