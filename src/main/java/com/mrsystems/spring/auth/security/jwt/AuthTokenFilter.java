e5f2cba082c87a1faec9f1e7738fac407e7cc931
MaikRibeiro <michael.douglas2020@hotmail.com>
2025-03-05 23:59:53 -0300

    Refactoring

--- src/main/java/com/mrsystems/spring/auth/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/mrsystems/spring/auth/security/jwt/AuthTokenFilter.java
@@ -34,7 +34,7 @@ public class AuthTokenFilter extends OncePerRequestFilter {
   }
 
 	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+	protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
 
 		try {
 			String jwt = parseJwt(request);
