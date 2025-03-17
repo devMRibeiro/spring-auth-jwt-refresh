4d437b78c03cb1a85cee7c659d2dcc46883f06de
devMRibeiro <michael.douglas2020@hotmail.com>
2025-03-17 01:41:38 -0300

    Add transactional annotation

--- src/main/java/com/mrsystems/spring/auth/security/service/UserDetailsServiceImpl.java
+++ src/main/java/com/mrsystems/spring/auth/security/service/UserDetailsServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mrsystems.spring.auth.model.User;
 import com.mrsystems.spring.auth.repository.UserRepository;
@@ -12,8 +13,13 @@ import com.mrsystems.spring.auth.repository.UserRepository;
 public class UserDetailsServiceImpl implements UserDetailsService {
 
 	private UserRepository userRespository;
+
+	UserDetailsServiceImpl(UserRepository userRespository) {
+		this.userRespository = userRespository;
+	}
 	
 	@Override
+	@Transactional
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 	
 		User user = userRespository.findByUsername(username)
